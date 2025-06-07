baf229988b69d0f716f9876bb109f66f6da46cb6
jack-thesparrow <rahultudu4705@gmail.com>
2025-06-07 21:58:46 +0530

    added lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -5,11 +5,19 @@
     lsp = {
       enable = true;
       lspconfig.enable = true;
+      formatOnSave = true;
 
       servers = {
         nixd = {
           filetypes = [ "nix" ];
           cmd = [ "${pkgs.nixd}/bin/nixd" ];
+          settings = {
+            nixd = {
+              formatting = {
+                command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+              };
+            };
+          };
         };
 
         clangd = {
@@ -19,14 +27,37 @@
           ];
           cmd = [ "${pkgs.clang-tools}/bin/clangd" ];
         };
+
+        pylsp = {
+          filetypes = [ "python" ];
+          cmd = [
+            "${pkgs.python311Packages.python-lsp-server}/bin/pylsp"
+          ];
+          settings = {
+            pylsp.plugins = {
+              mypy = {
+                enabled = true;
+                live_mode = false;
+              };
+              #Linting
+              flake8.enable = true;
+
+              # Formatter
+              black.enable = true;
+
+              # Refractor
+              rope.enable = true;
+            };
+          };
+        };
       };
     };
 
     languages = {
       bash.lsp.enable = true;
-      clang.lsp.enable = true;
+      clang.lsp.enable = false;
       python.lsp.enable = false;
-      nix.lsp.enable = true;
+      nix.lsp.enable = false;
     };
   };
 }
