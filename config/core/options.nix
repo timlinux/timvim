d925f36f1a4aef07bf713eea47bce28d76b1de5c
jack-thesparrow <rahultudu4705@gmail.com>
2025-06-07 10:26:12 +0530

    added the indentations settings

--- config/core/options.nix
+++ config/core/options.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }:
 {
   vim.options = {
+    # Numbering
     number = true;
     relativenumber = true;
 
+    # Tab Settings
     tabstop = 2;
     softtabstop = 2;
-    shiftwidth = 2;
     showtabline = 2;
     expandtab = true;
+
+    # Indentation
+    smartindent = true;
+    shiftwidth = 2;
+    breakindent = true;
+
+    wrap = false;
   };
 }
