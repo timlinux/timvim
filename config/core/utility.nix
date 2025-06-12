df3f4f5d02b25437a8aefc83226dbcce7b03744d
jack-thesparrow <rahultudu4705@gmail.com>
2025-06-12 12:07:52 +0530

    added image utilities

--- config/core/utility.nix
+++ config/core/utility.nix
@@ -12,5 +12,23 @@
       };
     };
     snacks-nvim.enable = true;
+
+    images = {
+      image-nvim = {
+        enable = true;
+        setupOpts = {
+          backend = "kitty";
+          editorOnlyRenderWhenFocused = false;
+          integrations = {
+            markdown = {
+              enable = true;
+              clearInInsertMode = true;
+              downloadRemoteImages = true;
+            };
+          };
+        };
+      };
+      img-clip.enable = true;
+    };
   };
 }
