3c373a0ae23ec4a174444f35666c86fc16ff8c72
Jack The Sparrow <rahultudu4705@gmail.com>
2025-06-18 17:19:21 +0530

    Added Debugger Mappings (#12)
    * added minimap and aerial
    
    * added debugger mappings

--- config/plugins/debugger.nix
+++ config/plugins/debugger.nix
@@ -11,9 +11,19 @@
 
       mappings = {
         continue = "<leader>dc";
-        goDown = "<leader>dj";
-        goUp = "<leader>dk";
+        goDown = "<leader>dvi";
+        goUp = "<leader>dvo";
         hover = "<leader>dh";
+        restart = "<leader>dR";
+        runLast = "<leader>d.";
+        runToCursor = "<leader>dgk";
+        stepBack = "<leader>dgk";
+        stepInto = "<leader>dgo";
+        stepOver = "<leader>dgj";
+        terminate = "<leader>dq";
+        toggleBreakpoint = "<leader>db";
+        toggleDapUI = "<leader>du";
+        toggleRepl = "<leader>dr";
       };
     };
   };
