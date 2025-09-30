3e48a4d8090d6f4a60400c70d1c3fda7190e4da9
Tim Sutton <tim@kartoza.com>
2025-09-30 22:04:22 +0100

    Trying to make hints work

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -27,7 +27,15 @@
     };
     # Source custom Lua for lspsaga CursorHold diagnostics popup
     luaConfigRC.lspsaga_cursorhold = ''
-      require("config.plugins.lspsaga-cursorhold")
+            vim.api.nvim_create_autocmd("CursorHold", {
+        pattern = "*",
+        callback = function()
+          if vim.fn.mode() == "n" then
+            vim.cmd("Lspsaga show_line_diagnostics ++unfocus")
+          end
+        end,
+      })
+      vim.o.updatetime = 500  -- Faster CursorHold
     '';
   };
 }
--- config/plugins/lspsaga-cursorhold.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("CursorHold", {
-  pattern = "*",
-  callback = function()
-    if vim.fn.mode() == "n" then
-      vim.cmd("Lspsaga show_line_diagnostics ++unfocus")
-    end
-  end,
-})
-vim.o.updatetime = 500  -- Faster CursorHold
