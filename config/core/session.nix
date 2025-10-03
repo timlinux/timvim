bdccce66fa428d04a2e89e8d8dd65d2610695bb9
Tim Sutton <tim@kartoza.com>
2025-10-03 22:32:48 +0100

    Session management improvements

--- config/core/session.nix
+++ config/core/session.nix
@@ -1,32 +1,38 @@
 { ... }:
 {
-  vim.session.nvim-session-manager = {
-    enable = true;
-    usePicker = true;
-    mappings = {
-      deleteSession = "<leader>sd";
-      loadLastSession = "<leader>sl";
-      saveCurrentSession = "<leader>ss";
-      loadSession = "<leader>so";
-
+  vim = {
+    options = {
+      sessionoptions = "buffers,curdir,tabpages,winsize,help,winpos,winsize";
     };
-    setupOpts = {
-      autoload_mode = "CurrentDir"; # Possible values: Disabled, CurrentDir, LastSession, GitSession
-      autosave_last_session = true;
-      autosave_ignore_buftypes = [
-        "terminal"
-        "quickfix"
-        "nofile"
-        "help"
-      ];
 
-      autosave_ignore_dirs = [
-        "~/"
-        "~/Projects"
-        "~/Downloads/"
-        "~/temp"
-        "/tmp"
-      ];
+    session.nvim-session-manager = {
+      enable = true;
+      usePicker = true;
+
+      mappings = {
+        deleteSession = "<leader>sd";
+        loadLastSession = "<leader>sl";
+        saveCurrentSession = "<leader>ss";
+        loadSession = "<leader>so";
+      };
+
+      setupOpts = {
+        autoload_mode = "CurrentDir";
+        autosave_last_session = true;
+        autosave_ignore_buftypes = [
+          "terminal"
+          "quickfix"
+          "nofile"
+          "help"
+        ];
+        autosave_ignore_dirs = [
+          "~/"
+          "~/Projects"
+          "~/Downloads/"
+          "~/temp"
+          "/tmp"
+        ];
+      };
     };
   };
 }
