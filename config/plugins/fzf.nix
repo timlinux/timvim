622b3507b3252db84aab57f997789da9c690b2d3
Tim Sutton <tim@kartoza.com>
2026-02-17 17:16:37 +0000

    Configure fzf-lua to exclude .git folder from searches
    Add setupOpts to configure fd and ripgrep to ignore .git directories
    in file and grep operations.

--- config/plugins/fzf.nix
+++ config/plugins/fzf.nix
@@ -3,6 +3,14 @@
   vim = {
     fzf-lua = {
       enable = true;
+      setupOpts = {
+        files = {
+          fd_opts = "--color=never --type f --hidden --follow --exclude .git";
+        };
+        grep = {
+          rg_opts = "--color=never --hidden --no-heading --with-filename --line-number --column --smart-case --glob '!.git'";
+        };
+      };
     };
   };
 }
