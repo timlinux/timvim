8e6dc5e628871cd7386e2438538fa5fbf3dc5480
Tim Sutton <tim@kartoza.com>
2026-03-26 08:00:39 +0000

    Hide outline for unsupported doc types

--- config/utility/outline.nix
+++ config/utility/outline.nix
@@ -37,5 +37,41 @@
         end, 500)
       end,
     })
+
+    -- Close Aerial when entering buffers that don't support outline
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function(args)
+        local bufnr = args.buf
+        vim.defer_fn(function()
+          if not vim.api.nvim_buf_is_valid(bufnr) then
+            return
+          end
+          local filetype = vim.bo[bufnr].filetype
+          local buftype = vim.bo[bufnr].buftype
+
+          -- Check if this buffer has an LSP client attached
+          local clients = vim.lsp.get_clients({ bufnr = bufnr })
+          local has_lsp = #clients > 0
+
+          -- Close aerial for unsupported buffers (no LSP, special buffers, etc.)
+          local is_special = filetype == "alpha" or filetype == "" or
+                            filetype == "aerial" or filetype == "neo-tree" or
+                            filetype == "toggleterm" or filetype == "help" or
+                            buftype == "nofile" or buftype == "terminal" or
+                            buftype == "prompt" or buftype == "quickfix"
+
+          if is_special or not has_lsp then
+            local aerial = require("aerial")
+            if aerial.is_open() then
+              aerial.close()
+              _G.toggle_states['<leader>to'] = false
+              if _G.update_toggle_desc then
+                _G.update_toggle_desc('<leader>to', 'Code Outline', false)
+              end
+            end
+          end
+        end, 100)
+      end,
+    })
   '';
 }
